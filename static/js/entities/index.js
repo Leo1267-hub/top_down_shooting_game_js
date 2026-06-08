766133fdadc7ed33287a629b81b3301cd303e4a5
Leonid Berdin <leonidberdin1@gmail.com>
2026-06-08 17:14:29 +0100

    Add entity barrel exports

---
+++ static/js/entities/index.js
@@ -0,0 +1,9 @@
+export { GameObject, Game_object } from "./GameObject.js";
+export { Player } from "./Player.js";
+export { Enemy } from "./Enemy.js";
+export { ShootingEnemy, Shooting_enemy } from "./ShootingEnemy.js";
+export { Boss } from "./Boss.js";
+export { Plant } from "./Plant.js";
+export { Bullet } from "./Bullet.js";
+export { Mine } from "./Mine.js";
+export { AmmoPickup, HealthPickup, Ammo, Health_ammo } from "./Pickup.js";
