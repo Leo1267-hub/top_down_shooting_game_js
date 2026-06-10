2bbacaa2f13037f96bd46cad415e740e970cbc4a
Leonid Berdin <leonidberdin1@gmail.com>
2026-06-10 11:13:42 +0100

    Load legacy adapters before game runtime

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
 import "./js/main.js";
 import "./js/core/LegacyInputAdapter.js";
+import "./js/core/LegacyCollisionAdapter.js";
+import "./js/core/LegacyAssetAdapter.js";
+import "./js/systems/LegacySpawnAdapter.js";
 import "./js/legacyGame.js";
